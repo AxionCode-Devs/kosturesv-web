ce054c6124e2e1e5120587632b5837189e971fc8
Francisco Castellanos <rs.fcastellanos@gmail.com>
2026-04-23 16:00:20 -0600

    chore: initial project setup - React + Vite + TypeScript + Tailwind CSS v4

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen">
+      <header className="p-6">
+        <h1 className="text-2xl font-bold">KostureSV</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
